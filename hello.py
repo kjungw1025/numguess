800f8ef89e2bd0a49ac9091c06c36609a41b7ff4
kjungw1025 <kjungw1025@naver.com>
2023-01-09 16:22:48 +0900

    feat: Get guess number
    practice simple guess number

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("helloworld")
+from random import randint
+answer = randint(1, 100)
+
+name = input("what is your name? ")
+print("HI, {}".format(name))
+guess = int(input("what is your guess number? "))
+print("your guess: {}, answer: {}".format(guess, answer))
