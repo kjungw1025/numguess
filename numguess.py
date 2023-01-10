9d634d1ccbd428cd6e57e0dac2e904ebb680bad2
kjungw1025 <kjungw1025@naver.com>
2023-01-10 15:11:55 +0900

    feat: Add Comparing users guess with answer

--- numguess.py
+++ numguess.py
@@ -1,7 +1,16 @@
 from random import randint
+from time import sleep
 answer = randint(1, 100)
 
 name = input("what is your name? ")
 print("HI, {}".format(name))
-guess = int(input("what is your guess number? "))
-print("your guess: {}, answer: {}".format(guess, answer))
+guess = int(input("what is your guess number? (1 ~ 100) "))
+if (guess == answer):
+    for _ in range(3):
+        sleep(1)
+        print("*************")
+    print("Correct {}! The answer was {}".format(name, str(answer)))
+elif guess > answer:
+    print(f"Keep going, man~! That was too high. {name}..")
+elif guess < answer:
+    print(f"Keep going, man~! That was too low. {name}..")
