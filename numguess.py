f9f7de32ff5603bc281bda2e1e22ad4341b1c08b
kjungw1025 <kjungw1025@naver.com>
2023-01-11 16:06:08 +0900

    feat: Get guess number
    guess number (chance = 5)

--- numguess.py
+++ numguess.py
@@ -2,15 +2,24 @@ from random import randint
 from time import sleep
 answer = randint(1, 100)
 
-name = input("what is your name? ")
-print("HI, {}".format(name))
-guess = int(input("what is your guess number? (1 ~ 100) "))
-if (guess == answer):
-    for _ in range(3):
-        sleep(1)
-        print("*************")
-    print("Correct {}! The answer was {}".format(name, str(answer)))
-elif guess > answer:
-    print(f"Keep going, man~! That was too high. {name}..")
-elif guess < answer:
-    print(f"Keep going, man~! That was too low. {name}..")
+def numguess():
+  name = input("what is your name? ")
+  print("HI, {}".format(name))
+  chance = 5
+  while (chance > 0):
+      guess = int(input("what is your guess number? (1 ~ 100) "))
+      if (guess == answer):
+          for _ in range(3):
+              sleep(1)
+              print("*************")
+          print("Correct {}! The answer was {}".format(name, str(answer)))
+          return
+      elif guess > answer:
+          chance -= 1
+          print(f"Keep going, man~! That was too high. {name}..")
+      elif guess < answer:
+          chance -= 1
+          print(f"Keep going, man~! That was too low. {name}..")
+
+numguess()
+
